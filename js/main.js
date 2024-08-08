0a10604dc12aabdbb18b620edb5be894062f61d0
Joaquim <joaquimmaio93@outlook.com>
2024-08-08 14:39:02 +0000

    atualizado8agosto

--- js/main.js
+++ js/main.js
@@ -60,3 +60,73 @@ function redirectToIndex(){
       document.body.scrollTop = 0;
       document.documentElement.scrollTop = 0;
   }
+
+  // Funções para login e register
+
+  function checkIfChecked() {
+    var checkboxes = document.querySelectorAll('.allchecks');
+    var isChecked = false;
+    for (var i = 0; i < checkboxes.length; i++) {
+      if (checkboxes[i].checked) {
+        isChecked = true;
+        console.log('Hey it is'+i);
+      }
+    }
+    return isChecked;
+  }
+   
+  document.getElementById('fileInput').addEventListener('change', function(event) {
+    const file = event.target.files[0]; // Seleccionar ficheiro
+    if (file) {
+        const reader = new FileReader(); // Criar um objecto FileReader
+        reader.onload = function(e) {
+            const content = e.target.result; // Get the file content
+            console.log(content); // conteudo do ficheiro
+            const wordsArray = content.split(' '); // Fazer parsing por espaco
+            // content do ficheiro vai para um array com duas posicoes uma para user e outra para password
+            const parsedUser = wordsArray[0];
+            const parsedPass = wordsArray[1];
+   
+            // Output de resultados
+            console.log('User:', parsedUser);
+            console.log('Pass:', parsedPass);
+            document.getElementById('idUtilizador').innerHTML = parsedUser;
+            closeModal('staticBackdrop4');
+            //document.getElementById('fileContent').textContent = content; // Display the content
+        };
+        reader.readAsText(file); // Read the file as text
+    } else {
+        console.log('No file selected');
+    }
+  });
+   
+  function closeModal(modalFechar) {
+    // Get the modal element by its ID
+    const modalElement = document.getElementById(modalFechar);
+   
+    // Create a Bootstrap modal instance
+    const modal = bootstrap.Modal.getInstance(modalElement);
+   
+    // Use the hide method to close the modal
+    modal.hide();
+  }
+   
+  function fazerReg() {
+    const inputUser = document.getElementById('inputUtilizador');
+    let inputUsVal = inputUser.value;
+    const inputPass = document.getElementById('inputPasswd');
+    let inputPaVal = inputPass.value;
+    //alert(document.getElementById('inputUtilizador').value)
+    //alert(inputVal);
+    const data = new Blob([inputUsVal,' ',inputPaVal], { type: 'application/json' });
+    const link = document.createElement('a');
+    link.download = "db.json";
+    link.href = window.URL.createObjectURL(data);
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+    console.log("Written file!");
+    closeModal('staticBackdrop5');
+  }
+  
+  
